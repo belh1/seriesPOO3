ecd1476ec60e962685e30c6f213b35fd8547a266
isabel <108592685+belh1@users.noreply.github.com>
2022-09-13 17:40:33 -0300

    Add files via upload

---
+++ src/app/pages/cadastrar/cadastrar-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { CadastrarPage } from './cadastrar.page';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: CadastrarPage
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class CadastrarPageRoutingModule {}
---
+++ src/app/pages/home/home.page.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Serie } from 'src/app/models/serie';
+import { SerieService } from 'src/app/services/services.page';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  series: Serie[];
+
+  constructor(private router: Router, private serieService: SerieService) {
+    this.series = this.serieService.series;
+  }
+
+  irParaCadastrar() {
+    this.router.navigate(['/cadastrar']);
+  }
+
+  irParaDetalhar(serie: Serie) {
+    this.router.navigateByUrl('/detalhar', { state: { objeto: serie } });
+  }
+}
